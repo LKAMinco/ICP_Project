90b4295e49aeb39a848fb2a6f487bffa3571b793
JuliusJx <jakubjsmykal@gmail.com>
2022-04-26 20:55:45 +0200

    erconnect class created

---
+++ src/cpp/erconnect.cpp
@@ -0,0 +1,14 @@
+/**
+ * ICP Project 2022 
+ * @file erconnect.cpp
+ * @brief implementation of methods in Erconnect class
+ * @author Jakub Julius Smykal (xsmyka01)
+ * @author Milan Hrabovsky (xhrabo15)
+ */
+
+#include <erconnect.h>
+
+Erconnect::Erconnect(Entity *start, Entity *end){
+    startPoint = start;
+    endPoint = end;
+}
---
+++ src/cpp/erconnect.h
@@ -0,0 +1,29 @@
+/**
+ * ICP Project 2022 
+ * @file erconnect.h
+ * @brief header file for erconnect class
+ * @author Jakub Julius Smykal (xsmyka01)
+ * @author Milan Hrabovsky (xhrabo15)
+ */
+
+#ifndef ERCONNECT_H
+#define ERCONNECT_H
+
+#include <entity.h>
+
+/**
+ * @class Erconnect
+ * @brief class for attributes in ER entities
+ */ 
+class Erconnect {
+    private:
+        Entity *startPoint;
+        Entity *endPoint;
+    public:
+        /**
+         * @name Constructor
+         */
+        Erconnect(Entity *start, Entity *end);
+};
+
+#endif
