c7f8140f2f307d1da042ff5cf33e79f90f4349eb
LKAMinco <milanhrabovsky29@gmail.com>
2022-04-26 04:42:39 +0200

    Init repositary

---
+++ UmlEditor.cpp
@@ -0,0 +1,15 @@
+#include "UmlEditor.h"
+#include "ui_UmlEditor.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "UmlEditor.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
