00397a9ee321b41d6895ca039d5f787989ee31eb
JuliusJx <jakubjsmykal@gmail.com>
2022-04-26 19:52:14 +0200

    attribute class created

---
+++ src/cpp/attribute.cpp
@@ -0,0 +1,33 @@
+/**
+ * ICP Project 2022 
+ * @file attribute.cpp
+ * @brief implementation of methods in Attribute class
+ * @author Jakub Julius Smykal (xsmyka01)
+ * @author Milan Hrabovsky (xhrabo15)
+ */
+
+#include <attribute.h>
+
+Attribute::Attribute(){
+    modifier = new std::string;
+    name = new std::string;
+    type = new std::string;
+}
+
+Attribute::~Attribute(){
+    delete modifier;
+    delete name;
+    delete type;
+}
+
+void Attribute::updateModifier(std::string newMod){
+    *modifier = newMod;
+}
+
+void Attribute::updateName(std::string newName){
+    *name = newName;
+}
+
+void Attribute::updateType(std::string newType){
+    *name = newType;
+}
---
+++ src/cpp/attribute.h
@@ -0,0 +1,49 @@
+/**
+ * ICP Project 2022 
+ * @file attribute.h
+ * @brief header file for attribute class
+ * @author Jakub Julius Smykal (xsmyka01)
+ * @author Milan Hrabovsky (xhrabo15)
+ */
+
+#ifndef ATTRIBUTE_H
+#define ATTRIBUTE_H
+
+#include <string>
+
+/**
+ * @class Attribute
+ * @brief class for attributes in ER entities
+ */ 
+class Attribute {
+    private:
+        std::string *modifier; // +, -, #, ~
+        std::string *name;
+        std::string *type;
+    public:
+        /**
+         * @name Constructor
+         */
+        Attribute();
+        /**
+         * @name Destructor
+         */
+        ~Attribute();
+        /**
+         * @name Update Modifier
+         * @brief updates value in modifier string
+         */
+        void updateModifier(std::string newMod);
+        /**
+         * @name Update Name
+         * @brief updates value in name string
+         */
+        void updateName(std::string newName);
+        /**
+         * @name Update Type
+         * @brief updates value in type string
+         */
+        void updateType(std::string newType);
+};
+
+#endif
