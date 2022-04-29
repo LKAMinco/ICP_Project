172cebc5f9c90d0f5c2bd908166122c806664716
LKAMinco <milanhrabovsky29@gmail.com>
2022-04-29 03:30:13 +0200

    added add method to class entity

--- src/classentity.cpp
+++ src/classentity.cpp
@@ -2,10 +2,7 @@
 #include "ui_classentity.h"
 #include <QDebug>
 
-ClassEntity::ClassEntity(QWidget *parent) :
-    QFrame(parent),
-    ui(new Ui::ClassEntity)
-{
+ClassEntity::ClassEntity(QWidget *parent) :QFrame(parent),ui(new Ui::ClassEntity){
     ui->setupUi(this);
     this->move(QCursor::pos().x(),QCursor::pos().y());
     qDebug() << "mouse X" << QCursor::pos().x();
@@ -17,4 +14,29 @@ ClassEntity::ClassEntity(QWidget *parent) :
 ClassEntity::~ClassEntity()
 {
     delete ui;
+    //TODO dorobit destructor na methods
+}
+
+void ClassEntity::on_add_method_clicked(){
+    current_height += 20;
+    if (current_height + 20 - 4 > 222){
+        this->resize(208, current_height+24);
+    }
+    line_name = "line_" + std::to_string(num_of_lines+1);
+    QLineEdit *line_edit;
+    EntityLine *new_line = new EntityLine;
+    line_edit = new QLineEdit(this);
+    line_edit->setObjectName(QStringLiteral("line_x"));
+    line_edit->setGeometry(QRect(4, current_height, 200, 20));
+    line_edit->setFrame(true);
+    line_edit->show();
+    new_line->line_edit = line_edit;
+    entity_lines.push_back(new_line);
+}
+
+void ClassEntity::on_add_attrib_clicked()
+{
+    for(unsigned int i = 0; i < entity_lines.size();i++){
+        entity_lines[i]->line_edit->setStyleSheet(QStringLiteral("background-color: rgb(38, 0, 255);"));
+    }
 }
--- src/classentity.h
+++ src/classentity.h
@@ -2,12 +2,25 @@
 #define CLASSENTITY_H
 
 #include <QFrame>
+#include <string>
+#include <iostream>
+#include <QLineEdit>
+#include <vector>
 #include <QGraphicsSceneMouseEvent>
+
+class EntityLine{
+public:
+    bool is_argument = false;
+    int type = -1;
+    int operands = -1;
+    std::string name = "";
+    QLineEdit *line_edit;
+};
+
 
 namespace Ui {
 class ClassEntity;
 }
-
 class ClassEntity : public QFrame
 {
     Q_OBJECT
@@ -16,8 +29,19 @@ public:
     explicit ClassEntity(QWidget *parent = nullptr);
     ~ClassEntity();
 
+
+private slots:
+    void on_add_method_clicked();
+
+    void on_add_attrib_clicked();
+
 private:
     Ui::ClassEntity *ui;
+    unsigned int current_height = 74;
+    unsigned int num_of_lines = 0;
+    std::vector<EntityLine*> entity_lines;
+    std::string line_name = "";
+    int index_of_first_attrib = -1;
 };
 
 #endif // CLASSENTITY_H
